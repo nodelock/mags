cd960eeec2f0417cd2db6f90202067aacfe499a1
nodelock <662263@gmail.com>
2026-09-10 22:18:38 +0800

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-set -eu
+set -e
 
 # MAGS 会在 /tmp 中执行固定启动命令。
 # 将应用复制到当前工作目录，再执行 node index.js。
@@ -11,4 +11,9 @@ if [ -d /app/node_modules ]; then
   cp -R /app/node_modules /tmp/node_modules
 fi
 
-exec "$@"
+# 自动重启循环：node 进程退出后等 3 秒重启
+while true; do
+  "$@" || true
+  echo "[mags] App exited, restarting in 3s..."
+  sleep 3
+done
